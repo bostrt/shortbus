3d0b173215954d51503b983379e7fcecb1f6df78
Shay C <pussylicker@Valley Of the Dammed.(none)>
2011-03-14 22:29:02 -0400

    eddited walls(draw)

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include <SDL/SDL.h>
 #include <iostream>
 using namespace std;
-
+SDL_Surface* screen;
 #include "walls.cpp"
 int main ( int argc, char** argv )
 {
@@ -15,13 +15,16 @@ int main ( int argc, char** argv )
     SDL_Surface* horiz=NULL;
     SDL_Surface* vert=NULL;
     SDL_Surface* bus = NULL;
-    SDL_Surface* screen=NULL;
+   
+   Walls wallv("vert.bmp");
+   
     SDL_Init(SDL_INIT_EVERYTHING);
     bus=SDL_LoadBMP("bus.bmp");
     screen=SDL_SetVideoMode(800,600,32,SDL_HWSURFACE);
     Uint32 colorkey = SDL_MapRGB( bus->format, 0xFF, 0xFF, 0xFF );
     SDL_SetColorKey( bus, SDL_SRCCOLORKEY, colorkey );
     SDL_BlitSurface(bus,NULL,screen,NULL);
+    wallv.draw(50,50,32,64);
     SDL_Flip(screen);
     bool done=false;
     while(!done)
--- walls.cpp
+++ walls.cpp
@@ -6,6 +6,8 @@ public:
 SDL_Surface* wall;
 
 Walls(string filename);
+void draw(int xPos,int yPos,int width,int height);
+
 };
 
 Walls::Walls(string filename)
@@ -13,8 +15,17 @@ Walls::Walls(string filename)
 
 char *cfilename;
 
-sz = new char[filename.length() + 1];
+cfilename = new char[filename.length() + 1];
 strcpy(cfilename, filename.c_str());
 wall=SDL_LoadBMP(cfilename);
 
+}
+void Walls::draw(int xPos,int yPos,int width,int height)
+{
+SDL_Rect dest;
+dest.x=xPos;
+dest.y=yPos;
+dest.w=width;
+dest.h=height;
+SDL_BlitSurface(wall,NULL,screen,&dest);
 }
