57e31dd15467b4ff1d5b436637527a9086839216
Shay C <pussylicker@Valley Of the Dammed.(none)>
2011-03-14 22:46:25 -0400

    made white bg

--- main.cpp
+++ main.cpp
@@ -23,9 +23,11 @@ int main ( int argc, char** argv )
     SDL_Init(SDL_INIT_EVERYTHING);
     bus=SDL_LoadBMP("bus.bmp");
     screen=SDL_SetVideoMode(800,600,32,SDL_HWSURFACE);
+    SDL_FillRect( screen, &screen->clip_rect, SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) );
     Uint32 colorkey = SDL_MapRGB( bus->format, 0xFF, 0xFF, 0xFF );
     SDL_SetColorKey( bus, SDL_SRCCOLORKEY, colorkey );
     SDL_BlitSurface(bus,NULL,screen,NULL);
+   //x,y,width,height
     wallv.draw(50,50,32,64);
     SDL_Flip(screen);
     bool done=false;
