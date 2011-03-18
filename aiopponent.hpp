18311bfce540d740f5fdc8aa6e0a5a1389be0d58
Robert Bost <bostrt@gmail.com>
2011-03-18 09:52:26 -0400

    Added files

---
+++ aiopponent.hpp
@@ -0,0 +1,20 @@
+#include <SDL/SDL.h>
+#include <string>
+#include "model.hpp"
+#include <vector>
+using namespace std;
+class AiOpponent:public Model{
+public:
+    bool dead;
+    int getRotation();
+    void setRotation(int rotate);
+    bool flip;
+    int x,y,points;
+    SDL_Surface* image;
+    double velocity,direction;
+    AiOpponent(int x,int y,string filename,int pts);
+    virtual void update(std::vector<Model *> world);
+    void die(Model* model);
+    void checkCollisions();
+};
+
