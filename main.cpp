c9cd2d99d71865e55e5ed55f6f742dc3a6f9e7e4
Robert Bost <bostrt@appstate.edu>
2011-03-17 20:21:42 -0400

    Enabled key repeat!

--- main.cpp
+++ main.cpp
@@ -15,27 +15,29 @@ SDL_Surface* screen;
 int main ( int argc, char** argv )
 {
     // SDL_Event event;
-   // SDL_Surface* bus=SDL_LoadBMP("bus.bmp");
+    // SDL_Surface* bus=SDL_LoadBMP("bus.bmp");
     SDL_Surface* vert=NULL;
     Player *player = new Player();
     Wall wallv("vert.bmp");
     SDL_Event event;
-   SDL_Surface* bg = SDL_LoadBMP("bg.bmp");  
+    SDL_Surface* bg = SDL_LoadBMP("bg.bmp");  
     SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
     screen=SDL_SetVideoMode(800,600,32,SDL_HWSURFACE);
     SDL_WM_SetCaption("SHOЯTBUƧ","SHOЯTBUƧ");
-   // SDL_FillRect( screen, &screen->clip_rect, SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) );
-   // Uint32 colorkey = SDL_MapRGB( bus->format, 0xFF, 0xFF, 0xFF );
-   // SDL_SetColorKey( bus, SDL_SRCCOLORKEY, colorkey );
+    // SDL_FillRect( screen, &screen->clip_rect, SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) );
+    // Uint32 colorkey = SDL_MapRGB( bus->format, 0xFF, 0xFF, 0xFF );
+    // SDL_SetColorKey( bus, SDL_SRCCOLORKEY, colorkey );
     //SDL_BlitSurface(bus,NULL,screen,NULL);
     //x,y,width,height
     wallv.draw(740,500,32,64);
-
+    
     bool done=false;
     // See http://en.wikipedia.org/wiki/Game_programming#Game_structure
     while(!done){
         // Get user input.
         while(SDL_PollEvent(&event)){
+            SDL_PumpEvents();
             player->handleEvent(&event);
             if(event.type==SDL_QUIT){
                 done=true;
