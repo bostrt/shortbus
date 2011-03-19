eb4d898345779ceb1fb756f566925c5cd13b0f70
Shay C <cichockisb@appstate.edu>
2011-03-19 15:57:09 -0400

    stuff

--- SDL_collide.h
+++ SDL_collide.h
@@ -1,21 +1,21 @@
 /*
     SDL_Collide:  A 2D collision detection library for use with SDL
-    
+
     MIT License
     Copyright 2005-2006 SDL_collide Team
     http://sdl-collide.sourceforge.net
     All rights reserved.
-    
+
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
     in the Software without restriction, including without limitation the rights
     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
     copies of the Software, and to permit persons to whom the Software is
     furnished to do so, subject to the following conditions:
-    
+
     The above copyright notice and this permission notice shall be included in all
     copies or substantial portions of the Software.
-    
+
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,10 +23,10 @@
     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
-    
+
     Amir Taaki
     genjix@crystalspace3d.org
-    
+
     Rob Loach
     http://robloach.net
 */
@@ -65,8 +65,7 @@ int SDL_CollideTransparentPixel(SDL_Surface *surface , int u , int v);
  *
  * @return non-zero on collision, 0 otherwise
  */
-int SDL_CollidePixel(SDL_Surface *as , int ax , int ay ,
-                       SDL_Surface *bs , int bx , int by, int skip = 4);
+int SDL_CollidePixel(SDL_Surface *as , int ax , int ay ,SDL_Surface *bs , int bx , int by, int skip = 4);
 
 /**
  * Bounding-box collision between two surfaces.
